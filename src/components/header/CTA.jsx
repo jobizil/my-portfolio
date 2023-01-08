f72578e7e7c81d0a678f54d9dfe9fab7633e56cd
Job Ugbem <65118336+jobizil@users.noreply.github.com>
2023-01-08 23:34:18 +0100

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
 		<div className="cta">
 			<a
 				className="btn"
-				href="https://www.dropbox.com/s/rwp4b9rx2797cfm/Job-Ugbem-Resume.pdf?dl=0"
+				href="https://www.dropbox.com/s/y3pmc7oj8nvszyq/Ugbem-Job-cv.pdf?dl=0"
 				target="_blank"
 				rel="noopener noreferrer">
 				Preview CV
